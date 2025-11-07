98418da4c7a81a029ef917961e57cc8a62f97591
JanaTarekk <janatarekelkott@gmail.com>
2025-11-07 19:49:58 +0200

    Implemented PrimaryIndex for doctors and appointments

--- PrimaryIndex.cpp
+++ PrimaryIndex.cpp
@@ -1,26 +1,99 @@
-#include <bits/stdc++.h>
-using namespace std;
 #include "PrimaryIndex.h"
 
-// TODO: Team Member 2 (Primary Index) to implement this.
+int PrimaryIndex::binarySearch(const string& key) {
+    int left = 0;
+    int right = index.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (index[mid].key == key && !index[mid].deleted) {
+            return mid;
+        } else if (index[mid].key < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
 
 void PrimaryIndex::loadIndex(const string& filename) {
-    // TODO: Team Member 2 (Primary Index) to implement this.
+    index.clear();
+    availList.clear();
+
+    if (testingMode) {
+        ofstream clearFile(filename, ios::trunc);
+        clearFile.close();
+    }
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        ofstream newFile(filename);
+        newFile.close();
+        return;
+    }
+
+    string key;
+    int rrn;
+    char delMarker;
+
+    while (file >> key >> rrn >> delMarker) {
+        bool isDeleted = (delMarker == '*');
+        index.push_back({key, rrn, isDeleted});
+        if (isDeleted) availList.push_back(rrn);
+    }
+
+    file.close();
+    sort(index.begin(), index.end(), [](const auto &a, const auto &b){
+        return a.key < b.key;
+    });
 }
 
 void PrimaryIndex::saveIndex() {
-    // TODO: Team Member 2 (Primary Index) to implement this.
+    ofstream file(indexFilename);
+    for (const auto& entry : index) {
+        file << entry.key << " " << entry.rrn << " " << (entry.deleted ? "*" : "-") << endl;
+    }
+    file.close();
 }
 
 int PrimaryIndex::search(const string& key) {
-    // TODO: Team Member 2 (Primary Index) to implement this.
+    int pos = binarySearch(key);
+    if (pos != -1) return index[pos].rrn;
     return -1;
 }
 
 void PrimaryIndex::addEntry(const string& key, int rrn) {
-    // TODO: Team Member 2 (Primary Index) to implement this.
+    if (search(key) != -1) {
+        cout << "Error: Key '" << key << "' already exists in primary index!" << endl;
+        return;
+    }
+
+    if (!availList.empty()) {
+        int reusedRRN = availList.front();
+        availList.pop_front();
+        index.push_back({key, reusedRRN, false});
+        cout << "Successfully added key '" << key << "' at reused RRN " << reusedRRN << " to primary index." << endl;
+    } else {
+        index.push_back({key, rrn, false});
+        cout << "Successfully added key '" << key << "' with RRN " << rrn << " to primary index." << endl;
+    }
+
+    sort(index.begin(), index.end(), [](const auto &a, const auto &b){
+        return a.key < b.key;
+    });
 }
 
 void PrimaryIndex::deleteEntry(const string& key) {
-    // TODO: Team Member 2 (Primary Index) to implement this.
+    int pos = binarySearch(key);
+    if (pos == -1) {
+        cout << "Warning: Key '" << key << "' not found in primary index. Cannot delete." << endl;
+        return;
+    }
+
+    index[pos].deleted = true;
+    availList.push_back(index[pos].rrn);
+    cout << "Successfully deleted key '" << key << "' from primary index. RRN added to AVAIL LIST." << endl;
 }
--- PrimaryIndex.h
+++ PrimaryIndex.h
@@ -2,11 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Primary index maps primary key -> RRN
 class PrimaryIndex {
+private:
+    struct IndexEntry {
+        string key;
+        int rrn;
+        bool deleted;
+    };
+
+    vector<IndexEntry> index;
+    string indexFilename;
+    list<int> availList;
+    bool testingMode;
+
+    int binarySearch(const string& key);
+
 public:
+    PrimaryIndex(const string& filename, bool testing = false)
+        : indexFilename(filename), testingMode(testing) {
+        loadIndex(filename);
+    }
+
     void loadIndex(const string& filename);
     void saveIndex();
+
     int search(const string& key);
     void addEntry(const string& key, int rrn);
     void deleteEntry(const string& key);
