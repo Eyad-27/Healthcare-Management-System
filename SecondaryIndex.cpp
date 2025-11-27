b65fc9baade0ec6856d4455ee5a5ed411e93214d
Yassin Elsawy <yassinsawy@outlook.com>
2025-11-27 11:35:28 +0200

    added Secondary Index

--- SecondaryIndex.cpp
+++ SecondaryIndex.cpp
@@ -3,25 +3,130 @@ using namespace std;
 
 #include "SecondaryIndex.h"
 
-// TODO: Team Member 3 (Secondary Index) to implement this.
 
 void SecondaryIndex::loadIndex(const string& filename) {
-    // TODO: Team Member 3 (Secondary Index) to implement this.
+    indexHead = nullptr;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        ofstream newFile(filename);
+        newFile.close();
+        return;
+    }
+
+    string secondaryKey;
+    string primaryKey;
+
+    while (file >> secondaryKey >> primaryKey) {
+        addEntry(indexHead, secondaryKey, primaryKey);
+    }
+    file.close();
+
 }
 
 void SecondaryIndex::saveIndex() {
-    // TODO: Team Member 3 (Secondary Index) to implement this.
+    ofstream file(indexFilename);
+    SecIndex* curr = indexHead;
+
+    while (curr != nullptr) {
+        SecNode* pkNode = curr->head;
+        while (pkNode != nullptr) {
+            file << curr->secondaryKey << " " << pkNode->primaryKey << endl;
+            pkNode = pkNode->next;
+        }
+        curr = curr->next;
+    }
+
+    file.close();
 }
 
 vector<string> SecondaryIndex::search(const string& secondaryKey) {
-    // TODO: Team Member 3 (Secondary Index) to implement this.
-    return {};
+    vector<string> primaryKeys;
+    SecIndex* curr = indexHead;
+
+    while (curr != nullptr && curr->secondaryKey < secondaryKey) {
+        curr = curr->next;
+    }
+
+    if (curr != nullptr && curr->secondaryKey == secondaryKey) {
+        SecNode* pkNode = curr->head;
+        while (pkNode != nullptr) {
+            primaryKeys.push_back(to_string(pkNode->primaryKey));
+            pkNode = pkNode->next;
+        }
+    }
+
+    return primaryKeys;
 }
 
-void SecondaryIndex::addEntry(const string& secondaryKey, const string& primaryKey) {
-    // TODO: Team Member 3 (Secondary Index) to implement this.
+void SecondaryIndex::addEntry(SecIndex* &head, const string& secondaryKey, const string& primaryKey) {
+    if (head == nullptr) {
+        head = new SecIndex(secondaryKey);
+        head->head = new SecNode(stoi(primaryKey));
+        return;
+    }
+
+    SecIndex* curr = head;
+    SecIndex* prev = nullptr;
+
+    while (curr != nullptr && curr->secondaryKey < secondaryKey) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (curr != nullptr && curr->secondaryKey == secondaryKey) {
+        SecNode* pkNode = new SecNode(stoi(primaryKey));
+        pkNode->next = curr->head;
+        curr->head = pkNode;
+    } else {
+        SecIndex* newIndex = new SecIndex(secondaryKey);
+        newIndex->head = new SecNode(stoi(primaryKey));
+        if (prev == nullptr) {
+            newIndex->next = head;
+            head = newIndex;
+        } else {
+            newIndex->next = curr;
+            prev->next = newIndex;
+        }
+    }
+
 }
 
-void SecondaryIndex::deleteEntry(const string& secondaryKey, const string& primaryKey) {
-    // TODO: Team Member 3 (Secondary Index) to implement this.
+void SecondaryIndex::deleteEntry(SecIndex* &head, const string& secondaryKey, const string& primaryKey) {
+    SecIndex* curr = head;
+    SecIndex* prev = nullptr;
+
+    while (curr != nullptr && curr->secondaryKey < secondaryKey) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (curr != nullptr && curr->secondaryKey == secondaryKey) {
+        SecNode* pkCurr = curr->head;
+        SecNode* pkPrev = nullptr;
+        int pkToDelete = stoi(primaryKey);
+
+        while (pkCurr != nullptr && pkCurr->primaryKey != pkToDelete) {
+            pkPrev = pkCurr;
+            pkCurr = pkCurr->next;
+        }
+
+        if (pkCurr != nullptr) {
+            if (pkPrev == nullptr) {
+                curr->head = pkCurr->next;
+            } else {
+                pkPrev->next = pkCurr->next;
+            }
+            delete pkCurr;
+
+            if (curr->head == nullptr) {
+                if (prev == nullptr) {
+                    head = curr->next;
+                } else {
+                    prev->next = curr->next;
+                }
+                delete curr;
+            }
+        }
+    }
+
 }
--- SecondaryIndex.h
+++ SecondaryIndex.h
@@ -4,10 +4,28 @@ using namespace std;
 
 // Secondary index maps secondary key -> one or more primary keys (via linked list or vector placeholder)
 class SecondaryIndex {
+private:
+    struct SecNode{
+        int primaryKey;
+        SecNode* next;
+        SecNode(int pk) : primaryKey(pk), next(nullptr) {}
+    };
+    struct SecIndex {
+        string secondaryKey;
+        SecNode* head; // Head of linked list of primary keys
+        SecIndex* next;
+        SecIndex(const string& sk) : secondaryKey(sk), head(nullptr), next(nullptr) {}
+    };
+    SecIndex* indexHead;
+    string indexFilename;
 public:
+    SecondaryIndex(const string& filename)
+        : indexFilename(filename), indexHead(nullptr) {
+        loadIndex(filename);
+    }
     void loadIndex(const string& filename);
     void saveIndex();
-    vector<string> search(const string& secondaryKey);
-    void addEntry(const string& secondaryKey, const string& primaryKey);
-    void deleteEntry(const string& secondaryKey, const string& primaryKey);
+    vector<string> search(const string &secondaryKey);
+    void deleteEntry(SecIndex *&head, const string &secondaryKey, const string &primaryKey);
+    void addEntry(SecIndex *&head, const string &secondaryKey, const string &primaryKey);
 };
